f9e162e81172a97c9ad703c6697a1190c2ae8c5f
malav-greycell <107108667+malav-greycell@users.noreply.github.com>
2024-05-30 21:06:54 +1000

    Added email block in register screen.

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -11,6 +11,7 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool isVisible=false;
@@ -24,6 +25,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
         'Content-Type': 'application/json',
       },
       body: jsonEncode({
+        'email':_emailController.text,
         'username': _usernameController.text,
         'password': _passwordController.text,
         'role': _role,
@@ -33,8 +35,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
     if (response.statusCode == 201) {
       //ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Registration successful')));
       Navigator.of(context).pushReplacement(
-         MaterialPageRoute(builder: (context) => LoginScreen()),
-       );
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to register')));
     }
@@ -44,83 +46,40 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(5.0),
+        padding: EdgeInsets.all(0.0),
         child: Center(
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: <Widget>[
-                // TextField(
-                //   controller: _usernameController,
-                //   decoration: InputDecoration(labelText: 'Username'),
-                // ),
-                Image.asset('assets/images/signin_balls.png'),
-            const Text(
-                      'Create One!',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 50,
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    maxWidth: 400,
-                  ),
-                  child: TextFormField(
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    controller: _usernameController,
-                    validator: (value){
-                      if(value!.isEmpty){
-                        return "Username is required";
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      contentPadding: const EdgeInsets.all(27),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                          color: Pallete.borderColor,
-                          width: 3,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(
-                          color: Pallete.gradient2,
-                          width: 3,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      hintText: 'Username',
+          child: SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: <Widget>[
+                  // TextField(
+                  //   controller: _usernameController,
+                  //   decoration: InputDecoration(labelText: 'Username'),
+                  // ),
+                  Image.asset('assets/images/signin_balls.png'),
+                  const Text(
+                    'Create One!',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50,
                     ),
                   ),
-                ),
-                // TextField(
-                //   controller: _passwordController,
-                //   obscureText: true,
-                //   decoration: InputDecoration(labelText: 'Password'),
-                // ),
-                // ElevatedButton(
-                //   onPressed: _register,
-                //   child: Text('Register'),
-                // ),
-                const SizedBox(height: 15),
-                ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    maxWidth: 400,
-                  ),
-                  child: TextFormField(
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    controller: _passwordController,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Password is required';
-                      }
-                      return null;
-                    },
-                    obscureText: !isVisible,
-                    decoration: InputDecoration(
+                  const SizedBox(height: 10),
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxWidth: 400,
+                    ),
+                    child: TextFormField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      controller: _emailController,
+                      validator: (value){
+                        if(value!.isEmpty){
+                          return "Email is required";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
                         contentPadding: const EdgeInsets.all(27),
                         enabledBorder: OutlineInputBorder(
                           borderSide: const BorderSide(
@@ -136,104 +95,183 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           ),
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        hintText: 'Password',
-                        suffixIcon: IconButton(
-                            onPressed: () {
-                              //In here we will create a click to show and hide the password a toggle button
-                              setState(() {
-                                //toggle button
-                                isVisible = !isVisible;
-                              });
-                            },
-                            icon: Icon(isVisible
-                                ? Icons.visibility
-                                : Icons.visibility_off))
+                        hintText: 'Email',
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 15),
-                ConstrainedBox(
-                  constraints: const BoxConstraints(
-                    maxWidth: 400,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: RadioListTile<String>(
-                          title: const Text('Admin'),
-                          value: 'admin',
-                          groupValue: _role,
-                          onChanged: (value) {
-                            setState(() {
-                              _role = value;
-                            });
-                          },
+                  const SizedBox(height: 15),
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxWidth: 400,
+                    ),
+                    child: TextFormField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      controller: _usernameController,
+                      validator: (value){
+                        if(value!.isEmpty){
+                          return "Username is required";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.all(27),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Pallete.borderColor,
+                            width: 3,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                      ),
-                      Expanded(
-                        child: RadioListTile<String>(
-                          title: const Text('Student'),
-                          value: 'student',
-                          groupValue: _role,
-                          onChanged: (value) {
-                            setState(() {
-                              _role = value;
-                            });
-                          },
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Pallete.gradient2,
+                            width: 3,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
                         ),
+                        hintText: 'Username',
                       ),
-                    ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: 15),
-            Container(
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: [
-                    Pallete.gradient1,
-                    Pallete.gradient2,
-                    Pallete.gradient3,
-                  ],
-                  begin: Alignment.bottomLeft,
-                  end: Alignment.topRight,
-                ),
-                borderRadius: BorderRadius.circular(7),
-              ),
-              child: ElevatedButton(
-                onPressed: () {
-                  if(_role!.isEmpty){
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Role is not selected. Please choose one.')));
-                  }
-                  else{
-                    _register();
-                  }
-                },
-                style:
-                ElevatedButton.styleFrom(
-                  fixedSize: const Size(395, 55),
-                  shadowColor: Colors.transparent,
-                ),
-                child: Text(
-                  'Register',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 17,
+                  // TextField(
+                  //   controller: _passwordController,
+                  //   obscureText: true,
+                  //   decoration: InputDecoration(labelText: 'Password'),
+                  // ),
+                  // ElevatedButton(
+                  //   onPressed: _register,
+                  //   child: Text('Register'),
+                  // ),
+                  const SizedBox(height: 15),
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxWidth: 400,
+                    ),
+                    child: TextFormField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      controller: _passwordController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Password is required';
+                        }
+                        return null;
+                      },
+                      obscureText: !isVisible,
+                      decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(27),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Pallete.borderColor,
+                              width: 3,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Pallete.gradient2,
+                              width: 3,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          hintText: 'Password',
+                          suffixIcon: IconButton(
+                              onPressed: () {
+                                //In here we will create a click to show and hide the password a toggle button
+                                setState(() {
+                                  //toggle button
+                                  isVisible = !isVisible;
+                                });
+                              },
+                              icon: Icon(isVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off))
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-                const SizedBox(height: 15),
-            Row(
+                  const SizedBox(height: 15),
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxWidth: 400,
+                    ),
+                    child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        const Text("Already have an account"),
-                        TextButton(onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
-                        }, child: const Text("LOGIN")),
+                        Expanded(
+                          child: RadioListTile<String>(
+                            title: const Text('Admin'),
+                            value: 'admin',
+                            groupValue: _role,
+                            onChanged: (value) {
+                              setState(() {
+                                _role = value;
+                              });
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: RadioListTile<String>(
+                            title: const Text('Student'),
+                            value: 'student',
+                            groupValue: _role,
+                            onChanged: (value) {
+                              setState(() {
+                                _role = value;
+                              });
+                            },
+                          ),
+                        ),
                       ],
                     ),
-              ],
+                  ),
+                  const SizedBox(height: 15),
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [
+                          Pallete.gradient1,
+                          Pallete.gradient2,
+                          Pallete.gradient3,
+                        ],
+                        begin: Alignment.bottomLeft,
+                        end: Alignment.topRight,
+                      ),
+                      borderRadius: BorderRadius.circular(7),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if(_role!.isEmpty){
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Role is not selected. Please choose one.')));
+                        }
+                        else{
+                          _register();
+                        }
+                      },
+                      style:
+                      ElevatedButton.styleFrom(
+                        fixedSize: const Size(395, 55),
+                        shadowColor: Colors.transparent,
+                      ),
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Already have an account!"),
+                      TextButton(onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+                      }, child: const Text("LOGIN")),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
