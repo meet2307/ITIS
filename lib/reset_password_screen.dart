4c4a4affabf0f6cf2bb0911fa7ff9bd4e3a206be
malav-greycell <107108667+malav-greycell@users.noreply.github.com>
2024-05-31 16:30:35 +1000

    Changes in UI of reset password screens.

--- lib/reset_password_screen.dart
+++ lib/reset_password_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:itis_project_python/pallete.dart';
+
 class ResetPasswordTestScreen extends StatefulWidget {
   final String email;
   ResetPasswordTestScreen({required this.email});
@@ -13,6 +15,9 @@ class ResetPasswordTestScreen extends StatefulWidget {
 
 class _ResetPasswordTestScreenState extends State<ResetPasswordTestScreen> {
   final TextEditingController _passwordController = TextEditingController();
+  bool isVisible=false;
+  String _role = '';
+  final formKey = GlobalKey<FormState>();
 
   Future<void> resetPassword() async {
     var response = await http.post(
@@ -41,24 +46,108 @@ class _ResetPasswordTestScreenState extends State<ResetPasswordTestScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Reset Password"),
-      ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Enter your new password'),
-              obscureText: true,
+        padding: const EdgeInsets.all(0.0),
+        child: Center(
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset('assets/images/signin_balls.png'),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Reset Password!',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 50,
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    ConstrainedBox(
+                      constraints: const BoxConstraints(
+                        maxWidth: 400,
+                      ),
+                      child: TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: _passwordController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Password is required';
+                          }
+                          return null;
+                        },
+                        obscureText: !isVisible,
+                        decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.all(27),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                color: Pallete.borderColor,
+                                width: 3,
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                color: Pallete.gradient2,
+                                width: 3,
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            hintText: 'New Password',
+                            suffixIcon: IconButton(
+                                onPressed: () {
+                                  //In here we will create a click to show and hide the password a toggle button
+                                  setState(() {
+                                    //toggle button
+                                    isVisible = !isVisible;
+                                  });
+                                },
+                                icon: Icon(isVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off))
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container(
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [
+                            Pallete.gradient1,
+                            Pallete.gradient2,
+                            Pallete.gradient3,
+                          ],
+                          begin: Alignment.bottomLeft,
+                          end: Alignment.topRight,
+                        ),
+                        borderRadius: BorderRadius.circular(7),
+                      ),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          resetPassword();
+                        },
+                        style:
+                        ElevatedButton.styleFrom(
+                          fixedSize: const Size(395, 55),
+                          shadowColor: Colors.transparent,
+                        ),
+                        child: Text(
+                          'Reset',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: resetPassword,
-              child: Text('Reset Password'),
-            ),
-          ],
+          ),
         ),
       ),
     );
--- lib/send_reset_link.dart
+++ lib/send_reset_link.dart
@@ -1,6 +1,7 @@
 // send_reset_link_screen.dart
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:itis_project_python/pallete.dart';
 import 'dart:convert';
 
 import 'package:itis_project_python/reset_password_screen.dart';
@@ -13,6 +14,9 @@ class SendResetLinkScreen extends StatefulWidget {
 
 class _SendResetLinkScreenState extends State<SendResetLinkScreen> {
   final TextEditingController _emailController = TextEditingController();
+  bool isVisible=false;
+  String _role = '';
+  final formKey = GlobalKey<FormState>();
 
   Future<void> sendResetLink() async {
     ScaffoldMessenger.of(context).showSnackBar(
@@ -61,23 +65,96 @@ class _SendResetLinkScreenState extends State<SendResetLinkScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Send Reset Link"),
-      ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Enter your email'),
+        padding: const EdgeInsets.all(0.0),
+        child: Center(
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset('assets/images/signin_balls.png'),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Get link!',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 50,
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    ConstrainedBox(
+                      constraints: const BoxConstraints(
+                        maxWidth: 400,
+                      ),
+                      child: TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: _emailController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Email is required';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.all(27),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                color: Pallete.borderColor,
+                                width: 3,
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                color: Pallete.gradient2,
+                                width: 3,
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            hintText: 'Email',
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container(
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [
+                            Pallete.gradient1,
+                            Pallete.gradient2,
+                            Pallete.gradient3,
+                          ],
+                          begin: Alignment.bottomLeft,
+                          end: Alignment.topRight,
+                        ),
+                        borderRadius: BorderRadius.circular(7),
+                      ),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          sendResetLink();
+                        },
+                        style:
+                        ElevatedButton.styleFrom(
+                          fixedSize: const Size(395, 55),
+                          shadowColor: Colors.transparent,
+                        ),
+                        child: Text(
+                          'Send reset link',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: sendResetLink,
-              child: Text('Send Reset Link'),
-            ),
-          ],
+          ),
         ),
       ),
     );
